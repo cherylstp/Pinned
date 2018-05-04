94b9bdcdd9518d9a82f4fd8c45418c140e4ae155
jnuetzel21 <juli.nuetzel@gmail.com>
2018-05-04 14:12:58 -0400

    Login with twitter and streaming code

---
+++ my-pinned-app/sign_in_with_twitter/config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  twitterAuth: {
+    consumerKey: 'pxBAIvMpcAA1sEt9UT4vWI2aB',
+    consumerSecret: 'M0t73yIOaCrA8MsOev2vOFs4VjSnVh5DJYfutTIzLA2EoJL2Po',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+  }
+};
---
+++ my-pinned-app/sign_in_with_twitter/config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: 'mongodb://cherylstp:password12345@pinned-shard-00-00-dvbij.mongodb.net:27017,pinned-shard-00-01-dvbij.mongodb.net:27017,pinned-shard-00-02-dvbij.mongodb.net:27017/test?ssl=true&replicaSet=Pinned-shard-0&authSource=admin',
+};
